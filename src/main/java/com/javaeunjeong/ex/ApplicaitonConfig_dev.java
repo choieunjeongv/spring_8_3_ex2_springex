15126059058afbc7b2d09ba2880f4c65eae250c1
choieunheong <mjchoi17@hanmail.net>
2022-04-14 11:57:21 +0900

    8-3 JAVA설정 파일 불러오기

---
+++ src/main/java/com/javaeunjeong/ex/ApplicaitonConfig_dev.java
@@ -0,0 +1,23 @@
+package com.javaeunjeong.ex;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class ApplicaitonConfig_dev {
+
+	@Bean
+	public ServerInfo serverInfo(){
+		ServerInfo sInfo = new ServerInfo();
+		
+		sInfo.setIpNum("localhost");
+		sInfo.setPortNum("8888");
+		
+		return sInfo;
+		
+	}
+	
+	
+}
